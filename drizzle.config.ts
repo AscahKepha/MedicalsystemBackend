372a6cc624eb50b5e2dff7c1abddef3dfcda9789
AscahKepha <kephaascah00@gmail.com>
2025-07-31 13:02:54 +0300

    l

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,15 @@
 //drizzle-kit used for migrations,seeding,pushing..
 import "dotenv/config";
-import {defineConfig} from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
     dialect: "postgresql",
     schema: "./src/drizzle/schema.ts",
     out: "./src/drizzle/migrations",
-    dbCredentials:{
+    dbCredentials: {
         url: process.env.DATABASE_URL as string,
     },
     verbose: true, //means to enable or set to true detailed output in what is shown at the screen when logging or output
     strict: true,
 });
+//t
